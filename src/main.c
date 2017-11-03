1cf426166e34368afc69349a9831779ded9e49fe
J.T. Nunley <xtremegamesjt@gmail.com>
2017-11-03 16:38:24 -0700

    Added some initial files

---
+++ src/main.c
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  11/03/2017 04:36:42 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  J.T. Nunley (JaydomStudios), jtnunley01@gmail.com
+ *   Organization:  JaydomStudios
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+  puts("Welcome to Kelsier Version ALPHA.1");
+  return 0;
+}
