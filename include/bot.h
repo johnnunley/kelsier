5b8950d3b99014f67fb1a47bfb557706f3099ddb
J.T. Nunley <xtremegamesjt@gmail.com>
2017-11-07 12:42:25 -0800

    Yet another laptop commit

---
+++ include/bot.h
@@ -0,0 +1,28 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  bot.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  11/07/2017 12:40:49 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  J.T. Nunley (JaydomStudios), jtnunley01@gmail.com
+ *   Organization:  JaydomStudios
+ *
+ * =====================================================================================
+ */
+
+#ifndef BOT_H
+#define BOT_H
+
+#include "action.h"
+
+class KelsierBot { 
+
+};
+
+#endif
---
+++ include/util.h
@@ -0,0 +1,39 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  util.h
+ *
+ *    Description:  Utilites
+ *
+ *        Version:  1.0
+ *        Created:  11/07/2017 12:36:18 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  J.T. Nunley (JaydomStudios), jtnunley01@gmail.com
+ *   Organization:  JaydomStudios
+ *
+ * =====================================================================================
+ */
+
+#ifndef UTIL_H
+#define UTIL_H
+
+#include <vector>
+
+// helper classes
+template <class T1,class T2 = T1>
+class TwoVectors {
+  public:
+    vector<T1> vector1;
+    vector<T2> vector2;
+};
+
+// helper methods
+
+template <class T>
+TwoVectors<T> splitVector(vector<T> list, bool (*sorter)(T));
+
+
+
+#endif
